c4027bb5608a3af68e03243f0073512b569ee294
Hansen Hansen <hansen@LaptopvonHansen.fritz.box>
2024-04-10 16:05:35 +0200

    Port constant modified

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.get('/api/info',(req,res)=>{
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
